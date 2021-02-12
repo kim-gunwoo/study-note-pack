d33356ddf615a4494dc2d8c1390ad16906f4fac0
git-k <kgunu93@gmail.com>
2021-02-12 19:30:22 +0900

    commit

---
+++ 8.etc/etc.js
@@ -0,0 +1,18 @@
+/*
+서버리스
+
+serverless
+
+
+소켓
+
+Socket.io
+const socketio = websocket || polling;
+
+WebSocket.io
+
+
+스트라피
+Strapi
+
+*/
---
+++ 8.etc/strapi-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab6d02a7-8624-4295-9f6e-58b5f4ad5032'
+};
